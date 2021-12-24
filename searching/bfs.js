c5366fad89d060ac885a5149b28faffaa56bdf8f
Codhog <capetechen@gmail.com>
2021-12-24 10:24:07 -0500

    starting search bfs,dfs

---
+++ searching/bfs.js
@@ -0,0 +1,13 @@
+// /**
+//  * @param {number[][]} grid
+//  * @return {number}
+//  */
+//  var shortestPathBinaryMatrix = function(grid) {
+    
+// };
+class Amazon{
+    constructor(){
+        this.SEO = 'Jeff'
+        this.SVP
+    }
+}
