71ddfa6e41d58a6fd664a571d9620a4509973a0d
Codhog <capetechen@gmail.com>
2022-01-18 19:36:46 -0500

    mew flat method

--- Javascript/Array/rawFlat.js
+++ Javascript/Array/rawFlat.js
@@ -1,46 +1,75 @@
 // There are six ways to flatten an array 
 // 1. IterativeRecursive Method
-function recursiveFlat(arr, depth = 1) {
-  const res = []
-  for(let val of arr){
-    if(Array.isArray(val) && depth > 0){
-        res.push(...flat(val, depth - 1))
-    }
-    else{
-        res.push(val)
+// function recursiveFlat(arr, depth = 1) {
+//   const res = []
+//   for (let val of arr) {
+//     if (Array.isArray(val) && depth > 0) {
+//       res.push(...flat(val, depth - 1))
+//     }
+//     else {
+//       res.push(val)
 
-    }
-  }
-  return res
-}
+//     }
+//   }
+//   return res
+// }
 
 
-// 2. Iterative Method
+// // 2. Iterative Method
 
-const iterativeFlat = () =>{
+// const iterativeFlat = () => {
 
-}
+// }
 
-function flat(arr, depth = 1) {
+// function flat(arr, depth = 1) {
+//   const res = []
+//   const queue = [...arr]
+//   let flag = true
+//   while (flag) {
+//     while (depth > 0) {
+//       const head = queue.shift()
+//       if (Array.isArray(head)) {
+//         res.push(...head)
+//       }
+//       else {
+//         res.push(head)
+//       }
+//       depth--
+//       console.log(depth)
+//       flag = queue.some((val) => Array.isArray(val))
+
+//     }
+//     if (flag || depth < 1) {
+//       return res
+//     }
+//   }
+// }
+
+function bfsflat(arr, depth = 1) {
   // your imeplementation here
   const res = []
-  const queue = [].concat(arr)
-  let flag = false
-  while (true) {
-    while (queue.length) {
-      const item = queue.shift()
-      if (Array.isArray(item) && depth) {
-        res.push(...item)
-      } else {
-        res.push(item)
-      }
+  const queue = [...arr.map(val=>[val, 1])]
+
+  // console.log(queue);
+  while(queue.length){
+    const [element, elementDepth] = queue.shift()
+    if( Array.isArray(element) && elementDepth > 0){
+      queue.unshift(...element.map(elementValue=>[elementValue, elementDepth-1]))
     }
-    depth--
-    flag = res.some(item => Array.isArray(item))
-    if (!flag || !depth) {
-      return res
+    else{
+      res.push(element)
     }
-    queue.push(...res)
-    res.length = 0
   }
+  console.log(res);
 }
+
+
+const arr = [1, [2], [3, [4]]];
+
+bfsflat(arr)
+// [1, 2, 3, [4]]
+
+bfsflat(arr, 1)
+// [1, 2, 3, [4]]
+
+bfsflat(arr, 2)
---
+++ Javascript/Array/removeDuplicates.js
@@ -0,0 +1,9 @@
+let words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
+
+// const modifiedWords = words.filter( (word, index, arr) => {
+//   arr[index+1] +=' extra'
+//   return word.length < 6
+// })
+const modifiedWords = words
+modifiedWords[0] = 222
+console.log(words)
